d9d5676b61ef14082daa1f6cb4fb204792cb047a
github cjl248 <github cjl248@cornell.edu>
2020-12-29 11:14:51 -0500

    Getting appropriate response to /accounts

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var crypto = require('crypto');
+var fetch = require('node-fetch');
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -9,8 +12,61 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// javascripts setup
-app.use(express.static(path.join(__dirname, 'public')))
+
+
+// .env variables
+var result = require('dotenv').config()
+if (result.error) {
+  throw result.error
+} else {
+  const endpoint = "https://api.pro.coinbase.com"
+  app.set('api_key', result.parsed.API_KEY)
+  app.set('api_secret', result.parsed.API_SECRET)
+  app.set('api_passphrase', result.parsed.PASSPHRASE)
+
+  const API_KEY = result.parsed.API_KEY
+  const API_SECRET = result.parsed.API_SECRET
+  const PASSPHRASE = result.parsed.PASSPHRASE
+
+  const TIMESTAMP = Date.now() / 1000;
+  const requestPath = '/accounts'
+  const method = 'GET'
+  const body = ''
+
+  const message = TIMESTAMP + method + requestPath + body
+  console.log("message", message);
+
+  // decode the base64 secret
+  const KEY = Buffer(API_SECRET, 'base64')
+  // create a sha256 hmac with the secret
+  const HMAC = crypto.createHmac('sha256', KEY);
+  // sign the require message with the HMAC
+  // and finally base64 encode the result
+  const SIGNATURE = HMAC.update(message).digest('base64');
+
+  const config = {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json',
+      'CB-ACCESS-KEY': API_KEY,
+      'CB-ACCESS-SIGN': SIGNATURE,
+      'CB-ACCESS-TIMESTAMP': TIMESTAMP,
+      'CB-ACCESS-PASSPHRASE': PASSPHRASE,
+    }
+  }
+
+  console.log('endpoint', endpoint+requestPath)
+  console.log('config', config)
+
+  fetch(endpoint+requestPath, config)
+    .then(r => r.json())
+    .then(resp => {
+      console.log(resp)
+    })
+
+}
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express App' });
+
+  API_KEY = req.app.get('api_key')
+  API_SECRET = req.app.get('api_secret')
+  PASSPHRASE = req.app.get('passphrase')
+
 });
 
 module.exports = router;
